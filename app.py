90c34974e32275f521e7fa04fd13c2075582c0e6
nyasharonaldo <nyasharonaldo@gmail.com>
2021-01-12 06:25:20 +0000

    Working out kinks

--- app.py
+++ app.py
@@ -2,28 +2,63 @@ import threading
 import time
 from flask import Flask, jsonify, request
 from binance import Binance
+import numpy as np
+import pandas as pd
+import time
+
 
 
 app = Flask(__name__)
 global binance
+global stopLossActive
+global columns 
+columns=['base','quote','time_started','time_stopped','executed','stop','limit']
+stopLossActive = False
 binance = Binance()
 
 # export FLASK_APP=app.py
 # flask run
-# @app.before_first_request
-# def activate_job():
-#     def run_job():
-#         while True:
-#             print("Run recurring task")
-#             time.sleep(3)
+@app.before_first_request
+def activate_job():
+    global stopLossActive
+    def run_job():
+        while True:
+            if stopLossActive:
+                print("Run recurring task")
+                time.sleep(3)
 
-#     thread = threading.Thread(target=run_job)
-#     thread.start()
+    thread = threading.Thread(target=run_job)
+    thread.start()
 
 global stoploss
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+@app.route('/', methods=['POST'])
+def addStopLoss(base, quote, stopLossLevel):
+    columns=['base','quote','time_started','time_stopped','executed','stop','limit']
+    newStopLoss = {}
+    newStopLoss['base'] = base
+    newStopLoss['quote'] = quote
+    newStopLoss['time_started'] = int(time.time())
+    newStopLoss['time_stopped'] = np.NaN
+    newStopLoss['executed'] = False
+    newStopLoss['stop'] = stopLossLevel
+    newStopLoss['limit'] = np.NaN
+    df = pd.read_csv('stop_loss.csv')
+    # Get the last index of base 
+    # Check if it's been executed or stopped. If so continue if not, stop it and add a new one. (Delete the filtering)
+    df2 = df[(df['executed']==False)&(df['time_stopped']==np.NaN)&(df['base']==base)]
+    if len(df) == 1:
+        newStopLoss['time_started'] = df2.iloc[0]['time_started']
+        df.iloc[0] = newStopLoss
+    else:
+        df.append(newStopLoss,ignore_index=True)
+    df.to_csv('stop_loss.csv',index=False)
+    return 'Hello, World!'
+
+
 
 @app.route('/stoploss', methods=['POST'])
 def hasHitStoploss():
--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import pandas as pd
 import numpy as np 
 import schedule
 import time
-
+from app import addStopLoss
 
 
 
@@ -18,18 +18,21 @@ def run_test():
 
 
 if __name__=="__main__":
+    df = pd.DataFrame(columns=['base','quote','time_started','time_stopped','executed','stop','limit'])
+    df.to_csv('stop_loss.csv')
+    # addStopLoss('BTC','USDT','35000')
     
     # run_test()
     #TODO: Add if current candlestick is X percentage away from stop loss, close 
-    binance = Binance()
-    x = binance.GetAllOrderInfo('BTCUSDT')
-    df = pd.DataFrame(x)
-    df = binance.cleanData(df, 'time',[])
-    for row in range(0, len(df)):
-        print(df.iloc[row])
-    df2 = pd.DataFrame(binance.GetAccountInfo()['balances'])
-    df2['free'] = pd.to_numeric(df2['free'])
-    #TODO: We can use this to determine what positions we're in. Then we can enact stoploss if holding is above threshold
-    df2 = df2[df2['free']>0]
-    print(df2)
+    # binance = Binance()
+    # x = binance.GetAllOrderInfo('BTCUSDT')
+    # df = pd.DataFrame(x)
+    # df = binance.cleanData(df, 'time',[])
+    # for row in range(0, len(df)):
+    #     print(df.iloc[row])
+    # df2 = pd.DataFrame(binance.GetAccountInfo()['balances'])
+    # df2['free'] = pd.to_numeric(df2['free'])
+    # #TODO: We can use this to determine what positions we're in. Then we can enact stoploss if holding is above threshold
+    # df2 = df2[df2['free']>0]
+    # print(df2)
     
